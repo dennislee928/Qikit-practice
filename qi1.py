97ed1d0c9e5a122bc9d793b816b04ecb78ed38d9
dennislee928 <55692615+dennislee928@users.noreply.github.com>
2024-09-18 16:56:11 +0800

    ??

--- qi1.py
+++ qi1.py
@@ -1,4 +1,17 @@
-from qiskit import *   #import q-computing library
+from qiskit import Aer, execute  # 新增 execute 的匯入
+from qiskit import QuantumCircuit
+from setuptools import setup, find_packages  # type: ignore #import q-computing library
 circuit = QuantumCircuit(1, 1) #define input array to be(1, 1)
 circuit.h(0)#use Hadamard gate method to compress the input array
 circuit.measure([0], [0])#meaure the probabilities of compressed(kitsed) array to be (0,0)
+
+
+
+simulator = Aer.get_backend('qasm_simulator')  # 修改 per 為 Aer
+result = execute(circuit, simulator).result()
+
+
+
+
+counts = result.get_counts(circuit)
+print(counts) 
